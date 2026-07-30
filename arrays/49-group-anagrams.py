9d2d553505523dbcba10eca5d99bff6562f0e212
gian-dln <giandln7740@gmail.com>
2026-07-30 21:48:41 +0100

    49

--- arrays/49-group-anagrams.py
+++ arrays/49-group-anagrams.py
@@ -1,6 +1,17 @@
+from collections import defaultdict
+
+
 def groupAnagrams(strs: list[str]) -> list[list[str]]:
-    
-    return []
+    map = defaultdict(list)
+
+    for s in strs:
+        count = [0] * 26 #a-z
+
+        for c in s:  #count sort
+            count[ord(c) - ord('a')] += 1
+
+        map[tuple(count)].append(s)
 
 
+    return list(map.values())
 
