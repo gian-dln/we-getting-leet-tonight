252553d00a535c4b3838b12bb341233334c452a7
gian-dln <giandln7740@gmail.com>
2026-08-17 15:08:45 +0100

    74

--- binary-search/74-search-a-2d-matrix.py
+++ binary-search/74-search-a-2d-matrix.py
@@ -1,21 +1,32 @@
 def searchMatrix(matrix: list[list[int]], target: int) -> bool:
-    for i in range(len(matrix)):
-        if binarySearch(matrix[i], target) != -1:
+    rows, cols = len(matrix), len(matrix[0])
+
+    # binary search
+    top, bot = 0, rows
+    while top < bot:
+        row = (top+bot)//2
+        if target > matrix[row][-1]:
+            top = row + 1 #look at rows larger
+        elif target < matrix[row][0]:
+            bot = row
+        else:
+            break
+
+    if top >= rows:                 
+        return False
+    row = (top+bot)//2
+    l,r = 0, cols
+    while l<r:
+        m = (l+r)//2
+        if target>matrix[row][m]:
+            l = m+1
+        elif target<matrix[row][m]:
+            r = m
+        else:
             return True
-
-
 
     return False
 
-def binarySearch(list: list[int], target: int):
-    lo,hi = 0, len(list)
-    while lo<hi:
-        m = lo + ((hi-lo)//2)
-        if list[m] >= target:
-            hi = m
-        elif list[m] < target:
-            lo = m+1
-    return lo if lo<len(list) and list[lo] == target else -1
 
 
 matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]]
