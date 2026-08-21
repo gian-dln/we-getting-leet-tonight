31db6a1f73c21b34fc13da3ccfcdf4b3e6646c8b
gian-dln <giandln7740@gmail.com>
2026-08-21 13:57:13 +0100

    167

---
+++ two-pointers/167-two-sum-ii.py
@@ -0,0 +1,9 @@
+def twoSum(numbers: list[int], target: int) -> list[int]:
+    l,r = 0, len(numbers)-1
+    while numbers[l] + numbers[r] != target:
+        if numbers[l] + numbers[r] > target:
+            r-=1
+        if numbers[l] + numbers[r] < target:
+            l+=1
+
+    return [l+1, r+1]  
