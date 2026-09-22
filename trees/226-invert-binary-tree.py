2f5a864f35b38af7b9929dbd63af00dcd361324e
gian-dln <giandln7740@gmail.com>
2026-09-22 10:55:03 +0100

    226-dfsr

--- trees/226-invert-binary-tree.py
+++ trees/226-invert-binary-tree.py
@@ -9,17 +9,14 @@ class TreeNode:
         self.left = left
         self.right = right
 
-# dfs solution
+# dfs-recursive solution
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         if not root:
             return None
-        stack = [root]
-        while stack:
-            node = stack.pop()
-            node.left, node.right = node.right, node.left
-            if node.left:
-                stack.append(node.left)
-            if node.right:
-                stack.append(node.right)
+        root.left, root.right = root.right, root.left
+
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+
         return root
